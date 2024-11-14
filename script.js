a84d3a7807b78770811a169f3d25abb18cdd2448
rapip <rafifgenadratama28@gmail.com>
2024-11-14 10:51:35 +0700

    finish

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-function createGrids() {
+function createGrids(gridNum) {
   const container = document.querySelector(".container");
-  for (let i = 0; i < 16; i++) {
+
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  for (let i = 0; i < gridNum; i++) {
     const row = document.createElement("div")
     row.classList.add("row");
-    for (let i = 0; i < 16; i++) {
+    row.style.height = 100 / gridNum + "%";
+    console.log(row.style.height);
+    for (let j = 0; j < 16; j++) {
       const rowElm = document.createElement("div");
       rowElm.classList.add("row-elm");
+      rowElm.addEventListener("mouseenter", function () {
+        rowElm.style.backgroundColor = "black";
+      })
       row.appendChild(rowElm);
     }
     container.appendChild(row);
   }
 }
 
-createGrids();
+createGrids(16);
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+  let input = prompt("How many grid do you want to have?\nExample: 16 would result in 16x16");
+  while (input > 100) {
+    input = prompt("Your input is exceeding the maximum amount (100). Please type again.");
+  }
+  createGrids(input);
+});
